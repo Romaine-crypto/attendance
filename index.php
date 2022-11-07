cd5a15357bbf2acd3a00b97b053a70b6842ce589
Romaine-crypto <77922118+Romaine-crypto@users.noreply.github.com>
2022-11-07 01:30:39 -0500

    footer

---
+++ index.php
@@ -0,0 +1,67 @@
+   
+<?php 
+     $title= 'Index';
+     require_once 'includes/header.php'; 
+     require_once 'db/conn.php';
+
+     //Get Specialties
+     $results = $crud->getSpecialties();
+?>
+
+    <h1 class="text-center" >Registration for IT Conference</h1>
+
+        <form method="post" action ="success.php">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputfirstname">First Name</label>
+                    <input required type="text" class="form-control" id="firstname" placeholder="Please Enter First Name" name="firstname">
+                </div>
+                <div class="form-group col-md-6">
+                    <label for="inputlastname">Last Name</label>
+                    <input required type="text" class="form-control" id="lastname" placeholder="Please Enter Last Name" name="lastname">
+                </div>
+            </div>
+                <div class="form-group">
+                    <label for="dob">Date of Birth</label>
+                    <input type="text" class="form-control" id="dob" placeholder="Please Select DOB" name="dob">
+                </div>
+                <div class="form-group">
+                        <label for="SelectSpecialty">Specialty</label>
+                        <label class="mr-sm-2 sr-only" for="inlineFormCustomSelect">Preference</label>
+                        <select class="custom-select mr-sm-2" id="specialty" name="specialty">
+                            <?php while ($r = $results ->fetch (PDO::FETCH_ASSOC)) {?>
+                                <option value= "<?php echo $r['specialty_id'] ?>"><?php echo $r['name']; ?></option>
+
+                            <?php }?>
+                        </select>
+                </div>
+            
+                <div class="form-group">
+                    <label for="inputEmail4">Email Address</label>
+                    <input required type="email" class="form-control" id="email" placeholder="Enter Email Here"name="email">
+                    <small id="emailHelp" class="form-text text-muted"> We will never share your Email with anyone else.</small>
+                </div>
+                <div class="form-group">
+                        <label for="phone">Contact Number</label>
+                        <input type="text" class="form-control" id="phone" placeholder="Enter Phone Number Here"name="phone">
+                        <small id="phoneHelp" class="form-text text-muted"> We will never share your number with anyone else.</small>
+                </div>
+                
+                <div class="custom-file">
+                        <input type="file" accept="image/*" class="custom-file-input" id="avatar" name="avatar">
+                        <label class="custom-file-label" for = "avatar" >Choose Image</label>
+                        <small id="avatar" class="form-text text-yellow"> File uploading is Optional</small>
+                        
+                </div>
+        
+            
+
+    
+        <br>
+        <button type="Register" name="Register" class="btn btn-primary btn-block">Register</button>
+    
+    </form>
+    
+<br>
+<br>
+<?php require_once 'includes/footer.php'; ?>
